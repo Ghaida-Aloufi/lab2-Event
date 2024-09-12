cd8e20924c86de2833487928302903f0c9e0dca5
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-12 11:30:18 +0300

    add lab2

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+let x = document.getElementById("button1")
+
+x.addEventListener("click",()=>{
+  x.textContent="hello"
+})
+
+
+
+let y = document.getElementById("button2")
+
+y.addEventListener("click",()=>{
+   y.style.fontSize="20px";
+})
+
+let z = document.getElementById("button3")
+
+z.addEventListener("click",()=>{
+   z.style.fontSize="5px";
+})
+
+
+let p = document.getElementById("button4")
+
+p.addEventListener("click",()=>{
+   p.style.color="brown";
+})
+
+let w = document.getElementById("button5")
+
+w.addEventListener("click",()=>{
+   w.style.background="black";
+})
+
+
+
+let i = document.getElementById("button6")
+
+i.addEventListener("click",()=>{
+   document.body.style.backgroundImage=" url(https://cdn.tuwaiq.edu.sa/landing/images/logo/logo-h.png) "
+})
+
+
